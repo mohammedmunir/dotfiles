37d6826a39ab6fbd8bc663cd7fa317c2c71e4571
Mohammed Munir <mohammed.munir@gmail.com>
2024-05-13 19:03:06 +0100

    .

--- .setup/2-linkfiles.sh
+++ .setup/2-linkfiles.sh
@@ -10,7 +10,6 @@ source library.sh
 # linktarget="$4";
     
 _installSymLink nvim ~/.config/nvim ~/dotfiles/nvim/ ~/.config
-_installSymLink .xinitrc ~/.xinitrc ~/dotfiles/qtile/.xinitrc ~/.xinitrc   
 _installSymLink .bashrc-personal ~/.bashrc-personal ~/dotfiles/.bashrc-personal ~/.bashrc-personal
 
 echo "Symbolic links created."
