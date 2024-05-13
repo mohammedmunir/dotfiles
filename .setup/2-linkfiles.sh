ea90f373194b24fd1e4039b44cb9712d13d57840
Mohammed Munir <mohammed.munir@gmail.com>
2024-05-13 18:26:20 +0100

    update setup files

---
+++ .setup/2-linkfiles.sh
@@ -0,0 +1,17 @@
+# ------------------------------------------------------
+# Install dotfiles
+# ------------------------------------------------------
+
+source .install/library.sh
+
+# name="$1"
+# symlink="$2";
+# linksource="$3";
+# linktarget="$4";
+    
+_installSymLink nvim ~/.config/nvim ~/dotfiles/nvim/ ~/.config
+_installSymLink .xinitrc ~/.xinitrc ~/dotfiles/qtile/.xinitrc ~/.xinitrc   
+_installSymLink .bashrc-personal ~/.bashrc-personal ~/dotfiles/.bashrc-personal ~/.bashrc-personal
+
+echo "Symbolic links created."
+echo ""
