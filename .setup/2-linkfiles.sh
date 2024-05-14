13e716836f82161671e9a7b83696832a6a2e290c
mohammedmunir <mohammed.munir@gmail.com>
2024-05-14 11:15:14 +0100

    added more link files

--- .setup/2-linkfiles.sh
+++ .setup/2-linkfiles.sh
@@ -11,6 +11,9 @@ source library.sh
     
 _installSymLink nvim ~/.config/nvim ~/dotfiles/nvim/ ~/.config
 _installSymLink .bashrc-personal ~/.bashrc-personal ~/dotfiles/.bashrc-personal ~/.bashrc-personal
+_installSymLink starship ~/.config/starship.toml ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+_installSymLink rofi ~/.config/rofi ~/dotfiles/rofi/ ~/.config
+_installSymLink thunar ~/.config/thunar ~/dotfiles/thunar/ ~/.config
 
 echo "Symbolic links created."
 echo ""
