5c6807d71a49ca78e0688f5df4e39288267bd537
mohammedmunir <mohammed.munir@gmail.com>
2024-05-14 17:51:44 +0100

    update vm config

--- .setup/2-linkfiles.sh
+++ .setup/2-linkfiles.sh
@@ -13,7 +13,7 @@ _installSymLink nvim ~/.config/nvim ~/dotfiles/nvim/ ~/.config
 _installSymLink .bashrc-personal ~/.bashrc-personal ~/dotfiles/.bashrc-personal ~/.bashrc-personal
 _installSymLink starship ~/.config/starship.toml ~/dotfiles/starship/starship.toml ~/.config/starship.toml
 _installSymLink rofi ~/.config/rofi ~/dotfiles/rofi/ ~/.config
-_installSymLink thunar ~/.config/thunar ~/dotfiles/thunar/ ~/.config
+_installSymLink Thunar ~/.config/Thunar ~/dotfiles/Thunar/ ~/.config
 
 echo "Symbolic links created."
 echo ""
