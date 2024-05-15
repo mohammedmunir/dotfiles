54a65a59d7640c6920bd578cbdb053ddb8e7e4fe
mohammedmunir <mohammed.munir@gmail.com>
2024-05-15 07:53:39 +0100

    bookmarks for thunar

--- .setup/2-linkfiles.sh
+++ .setup/2-linkfiles.sh
@@ -13,7 +13,9 @@ _installSymLink nvim ~/.config/nvim ~/dotfiles/nvim/ ~/.config
 _installSymLink .bashrc-personal ~/.bashrc-personal ~/dotfiles/.bashrc-personal ~/.bashrc-personal
 _installSymLink starship ~/.config/starship.toml ~/dotfiles/starship/starship.toml ~/.config/starship.toml
 _installSymLink rofi ~/.config/rofi ~/dotfiles/rofi/ ~/.config
-_installSymLink Thunar ~/.config/Thunar ~/dotfiles/Thunar/ ~/.config
+_installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+_installSymLink .Xresources ~/.Xresources ~/dotfiles/gtk/.Xresources ~/.Xresources
+_installSymLink gtk-3.0 ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/   
 
 echo "Symbolic links created."
 echo ""
