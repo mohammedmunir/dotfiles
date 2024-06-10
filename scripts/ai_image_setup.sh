7d71e98eedee9121a143c551faddff39b83abd40
Mohammed Munir <mohammed.munir@gmail.com>
2024-06-10 16:29:47 +0100

    image ai

---
+++ scripts/ai_image_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://github.com/invoke-ai/InvokeAI/releases/tag/v4.2.4
+cd ~/Documents
+
+wget https://github.com/invoke-ai/InvokeAI/releases/download/v4.2.4/InvokeAI-installer-v4.2.4.zip
+
+
